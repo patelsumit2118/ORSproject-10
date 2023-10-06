e0613b06042d1a605464eb434d01384581526684
patelsumit2118 <772406spatel@gmail.com>
2023-10-06 17:32:01 +0530

    done

---
+++ Project-10/projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.FacultyDTO;
+/**
+ * @author Sumit Verma
+ *
+ */
+public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
+
+}
---
+++ Project-10/projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Sumit Verma
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
